d37c26d846bc205bd75c40c558dda69eae3bcba4
trngkn <trungkiennd38@gmail.com>
2025-05-26 09:19:11 +0700

    Update render-build.sh

--- render-build.sh
+++ render-build.sh
@@ -1,22 +1,14 @@
 #!/usr/bin/env bash
-# exit on error
-set -o errexit
+# exit on errorset -o errexit
 
-STORAGE_DIR=/opt/render/project/.render
+npm install
+# npm run build # uncomment if required
 
-if [[ ! -d $STORAGE_DIR/chrome ]]; then
-  echo "...Downloading Chrome"
-  mkdir -p $STORAGE_DIR/chrome
-  cd $STORAGE_DIR/chrome
-  wget -P ./ https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
-  dpkg -x ./google-chrome-stable_current_amd64.deb $STORAGE_DIR/chrome
-  rm ./google-chrome-stable_current_amd64.deb
-  cd $HOME/project/src # Make sure we return to where we were
-else
-  echo "...Using Chrome from cache"
+# Store/pull Puppeteer cache with build cache
+if [[ ! -d $PUPPETEER_CACHE_DIR ]]; then 
+  echo "...Copying Puppeteer Cache from Build Cache" 
+  cp -R $XDG_CACHE_HOME/puppeteer/ $PUPPETEER_CACHE_DIR
+else 
+  echo "...Storing Puppeteer Cache in Build Cache" 
+  cp -R $PUPPETEER_CACHE_DIR $XDG_CACHE_HOME
 fi
-
-# be sure to add Chromes location to the PATH as part of your Start Command
-# export PATH="${PATH}:/opt/render/project/.render/chrome/opt/google/chrome"
-
-# add your own build commands...
