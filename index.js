a05de14ef7658b69c9438efbeb76d2d4feba50f2
trngkn <trungkiennd38@gmail.com>
2025-06-02 17:31:22 +0700

    Update index.js

--- index.js
+++ index.js
@@ -62,7 +62,7 @@ async function handleCaptcha(page, retryCount = 0) {
     
     const genAI = new GoogleGenerativeAI(apiKey);
     const model = genAI.getGenerativeModel({
-      model: 'gemini-1.5-flash-latest',
+      model: 'gemini-2.0-flash-lite',
       safetySettings: [
         {
           category: HarmCategory.HARM_CATEGORY_HARASSMENT,
